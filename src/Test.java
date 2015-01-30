c84ccd8087be3d4a021a6bcd51d8d3c231fe7bb6
JaySwang <wangnjuse@gmail.com>
2015-01-30 14:30:46 +0000

    sheng

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,8 @@
 public class Test {
 	public static void main(String args[]){
 		System.out.printf("hello everyone,have a try on this project lyr wss");
-		
+		System.out.printf("hello everyone,have a try on this project lyr wss");
+
 
 
 
