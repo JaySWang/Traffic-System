1e7077ca9afad7b452e1355f311997e88dd635bb
LiuYiran <yiran.liu@kcl.ac.uk>
2015-01-30 15:16:06 +0000

    liu

--- src/Test.java
+++ src/Test.java
@@ -3,9 +3,6 @@ public class Test {
 	public static void main(String args[]){
 		System.out.printf("hello everyone,have a try on this project lyr wss");
 
-		System.out.printf("hello everyone,have a try on this project lyr wss");
-		System.out.printf("hello everyone,have a try on this project qtqtqtqtqtqtqt wss");
-
 
 
 
