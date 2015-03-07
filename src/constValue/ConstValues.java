b657fbea0b0d8436eb83c73b283cb87c64d0c19a
JaySwang <wangnjuse@gmail.com>
2015-03-07 10:15:28 +0000

    Add const values

--- src/constValue/ConstValues.java
+++ src/constValue/ConstValues.java
@@ -2,11 +2,23 @@ package constValue;
 
 public class ConstValues {
 
-public static int GREEN = 1;
+ //TrafficLight states
+ public static int GREEN = 1;
  public static int RED = 0;
- 
  public static int ON = 1;
  public static int OFF = 0;
+ 
+ 
+ //vehicle behaviors
+ public static int STOP = 0;
+ public static int RUN = 1;
+ public static int TURN_LEFT = 2;
+ public static int TURN_RIGHT = 3;
+ public static int SLOW_DOWN = 4;
+ public static int SPEED_UP = 5;
+ 
+ 
+ 
 
  
  
