63d2b2ed5d372f97793ae806a4784ea69a4b0514
JaySwang <wangnjuse@gmail.com>
2015-03-19 17:25:09 +0000

    Upload changes

---
+++ src/mapCondition/Exit.java
@@ -0,0 +1,28 @@
+package mapCondition;
+
+public class Exit {
+	int no;
+	int location_x;
+	int location_y;
+	
+	//count the number of passing vehicles
+	int count = 0;
+
+	public Exit(int no, int location_x, int location_y) {
+		super();
+		this.no = no;
+		this.location_x = location_x;
+		this.location_y = location_y;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	
+	
+}
---
+++ src/model/VehicleGenerator.java
@@ -0,0 +1,103 @@
+package model;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import constValue.ConstValues;
+import modelInterface.IVehicle;
+import modelInterface.IVehicleGenerator;
+import mapCondition.Entrance;
+
+
+public class VehicleGenerator extends Thread implements IVehicleGenerator {
+	 int intervalTime;
+     int state;
+	 
+     List<Entrance> entrances = new ArrayList<Entrance>();
+     
+     
+     
+     
+     
+	public VehicleGenerator(int intervalTime) {
+		super();
+		this.intervalTime = intervalTime*1000;
+		initial();
+	}
+	
+	
+	
+
+	private void initial() {
+		Entrance e1 = new Entrance(0,0,160,ConstValues.EastToWest);
+		entrances.add(e1);
+		Entrance e2 = new Entrance(1,1200,200,ConstValues.WastToEest);
+		entrances.add(e2);
+	}
+
+
+
+
+	@Override
+	public void on() {
+		state = ConstValues.ON;
+		
+		start();
+	}
+
+	@Override
+	public void off() {
+		state = ConstValues.OFF;
+		
+	}
+
+
+	public void run(){
+		
+while(true){
+	 
+				
+		Vehicle v1 = new Vehicle();
+		v1.setLength(10);
+		v1.setWidth(10);
+		v1.setSpeed(10);
+        v1.setAcceleration(10);
+        
+        Entrance enrance;
+        int no = (int) (Math.random()*entrances.size());
+       enrance = entrances.get(no);
+	//	if(createOrNot()){
+	
+	    v1.setDirection(enrance.getDirection());
+		v1.setLocation_x(enrance.getLocation_x());
+		v1.setLocation_y(enrance.getLocation_y());
+		VehicleManagement.getInstance().addVehicle(v1);
+	//	}		
+		
+		try {
+					sleep(intervalTime);
+				} catch (InterruptedException e) {
+				
+					e.printStackTrace();
+				}
+				
+		
+			
+		
+			
+		}
+		
+}
+
+
+
+
+	private boolean createOrNot() {
+    if(Math.random()>0.5)
+    	return true;
+		
+		return false;
+	}
+
+}
