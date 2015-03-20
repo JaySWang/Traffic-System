7a5cd8511d002ca3798075f7804228735dc5a868
k1455365 <tong.qu@kcl.ac.uk>
2015-03-20 01:08:38 +0000

    change CrossTuring to judgementFlags add the flag used to judge the car on emergency stop satus or not

--- src/model/judgementFlags.java
+++ src/model/judgementFlags.java
@@ -4,8 +4,17 @@ package model;
  * @author qutong
  *
  */
-public class CrossTuring {
+public class judgementFlags {
 boolean east_west;
+boolean emergency;//show the car if emergency stop status or not.
+public boolean isEmergency() {
+	return emergency;
+}
+
+public void setEmergency(boolean emergency) {
+	this.emergency = emergency;
+}
+
 /**
  * if east_west==true then vehicle with angle 90 and 270 go first
  * once this vehicle pass set this flag to false
